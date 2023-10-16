ee4eaf9374b76d680f6b8c73086362e89b57de98
LaibaImran25 <120197378+LaibaImran25@users.noreply.github.com>
2023-10-16 14:17:33 +0500

    Update myapp012.java

--- myapp012.java
+++ myapp012.java
@@ -4,8 +4,8 @@ public class myapp012 {
         String RegNo = "SP21-BCT-012"; //this is my registeration number.
         int semester = 6;
 
-        System.out.println("Name: " + name);
-        System.out.println("Registration Number: " + regNo);
+        System.out.println("Name: " + Name);
+        System.out.println("Registration Number: " + RegNo);
         System.out.println("Semester: " + semester);
     }
 }
