82c17146ccca8238c74b152a0a4ff1cf44bbd9e6
rabbiya <100998044+Rabbiya-tabassum@users.noreply.github.com>
2023-10-16 14:29:00 +0500

    Update myapp021.java

--- myapp021.java
+++ myapp021.java
@@ -1,5 +1,5 @@
 public class myapp021 {
-    public void printInfo () {
+    public void printInfo() {
         String name = "Rabbiya Tabassum";
         String regNo = "SP21-BCT-021";
         int semester = 6;
