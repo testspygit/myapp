a1d43e88b71858a550fb94279f2963f8a08e6f38
LaibaImran25 <laibaimran0502@gmail.com>
2023-10-16 03:37:18 -0500

    myapp012.java commit

---
+++ myapp012.java
@@ -0,0 +1,12 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String Name = "LAIBA IMRAN";
+        String RegNo = "SP21-BCT-012";
+        int semester = 6;
+
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Semester: " + semester + "th");
+    }
+}
+# myapp
