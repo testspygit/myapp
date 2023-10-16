7204f768b2652c4016c8e6a8f80c8f71f24f3b7d
Muhammad Fawad <96443886+fawad0x0@users.noreply.github.com>
2023-10-16 14:12:15 +0500

    Update myapp016.java

--- myapp016.java
+++ myapp016.java
@@ -1,10 +1,8 @@
-//myapp
 public class myapp016 {
     public void printinfo(String[] args) {
         String name = "Muhammad Fawad Mushtaq";
         String regNo = "SP21-BCT-016";
         int semester = 6;
-
         System.out.println("Name: " + name);
         System.out.println("Registration Number: " + regNo);
         System.out.println("Semester: " + semester + "th");
