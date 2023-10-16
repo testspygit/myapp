e6b387871130bd7fcef499a180342062de596e2f
rabbiya <100998044+Rabbiya-tabassum@users.noreply.github.com>
2023-10-16 14:17:57 +0500

    Update myapp016.java

--- myapp016.java
+++ myapp016.java
@@ -5,6 +5,6 @@ public class myapp016 {
         int semester = 6;
         System.out.println("Name: " + name);
         System.out.println("Registration Number: " + regNo);
-        System.out.println("Semester: " + semester + " th")
+        System.out.println("Semester: " + semester +"th");
     }
 }
