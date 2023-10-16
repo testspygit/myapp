096f9f9f59f159e066b54983e169783ce90dafb5
malik-qasim <66017328+malik-qasim@users.noreply.github.com>
2023-10-16 13:51:40 +0500

    Update myapp.java

--- myapp.java
+++ myapp.java
@@ -1,13 +1,7 @@
 class myapp{
 	public static void main(String[] args){
-		myapp002 obj002 = new myapp002();
-		obj002.printInfo();
-		myapp003 obj003 = new myapp003();
-		obj003.printInfo();
 		myapp005 obj005 = new myapp005();
 		obj005.printInfo();
-		myapp008 obj008 = new myapp008();
-		obj008.printInfo();
 		myapp011 obj011 = new myapp011();
 		obj011.printInfo();
 		myapp012 obj012 = new myapp012();
@@ -16,12 +10,6 @@ class myapp{
 		obj013.printInfo();
 		myapp016 obj016 = new myapp016();
 		obj016.printInfo();
-		myapp017 obj017 = new myapp017();
-		obj017.printInfo();
-		myapp018 obj018 = new myapp018();
-		obj018.printInfo();
-		myapp020 obj020 = new myapp020();
-		obj020.printInfo();
 		myapp021 obj021 = new myapp021();
 		obj021.printInfo();
 		myapp027 obj027 = new myapp027();
