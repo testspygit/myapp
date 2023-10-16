c1aec3b74795338cf01b923f1ed331d90078ce7a
khadijabutt11 <138534011+khadijabutt11@users.noreply.github.com>
2023-10-16 14:21:11 +0500

    Update myapp011.java

--- myapp011.java
+++ myapp011.java
@@ -1,19 +1,12 @@
 public class myapp011 {
-    private String name;
-    private String id;
+   
 
-    public myapp011(String name, String id) {
-        this.name = name;
-        this.id = id;
+    public myapp011() {
     }
 
-    public void displayInfo() {
-        System.out.println(name + ", " + id);
+    public void printInfo() {
+        System.out.println("khadija ","Sp21-bct-011");
     }
 
-    public static void main(String[] args) {
-        myapp011 student = new myapp011("Khadija", "SP21-BCT-011");
-        student.displayInfo();
-    }
 }
 
