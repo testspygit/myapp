6c2aed0834b2db148d5d4e3bbae9c66e1fbc218f
Muhammad Fawad Mushtaq <fawad0x0@gmail.com>
2023-10-16 12:40:52 +0500

    Fawad's app

---
+++ myapp016.java
@@ -0,0 +1,11 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Muhammad Fawad Mushtaq";
+        String regNo = "SP21-BCT-016";
+        int semester = 6;
+
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Semester: " + semester + "th");
+    }
+}
