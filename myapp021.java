a53c9062acb48c9b709d595985adf46bf8164b28
rabbiya tabassum <rabbiyatabassum@gmail.com>
2023-10-16 04:42:34 -0400

    sp21-bct-021-first-commit

---
+++ myapp021.java
@@ -0,0 +1,11 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "LAIBA IMRAN";
+        String regNo = "SP21-BCT-012";
+        int semester = 6;
+
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Semester: " + semester + "th");
+    }
+}
