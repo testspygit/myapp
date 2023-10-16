9c54766cd23007b2bef9eea69d7e435e753073a8
Arsal-Amin <arslanamin434@gmail.com>
2023-10-16 08:36:01 +0000

    005 added

---
+++ myapp005.java
@@ -0,0 +1,8 @@
+public class  {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Arslan Amin");
+	System.out.println("Registration No. is  SP21-BCT-005" );
+	System.out.println("Semester is 6th");
+	    }
+}
+
