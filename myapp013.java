e1c2f3a1f5c0b172f540415d1f4fa8c2dcaced76
maheer-bit <maheer.2080@gmail.com>
2023-10-16 13:46:39 +0500

    013 added

---
+++ myapp013.java
@@ -0,0 +1,12 @@
+public class myapp013 {
+    public static void main(String[] args) {
+        String name = "Maheer Arshad";
+        String regNo = "SP21-BCT-013";
+        int semester = 6;
+
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Semester: " + semester + "th");
+    }
+}
+
